973e86b77aa6319b2e3839caef50a929a8501df0
andreja <ap9387@student.uni-lj.si>
2018-11-29 07:01:51 +0100

    dopolnjen statičen seznam

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import * as ReactDOM from "react-dom";
+
+
+function Contact(props) {
+    return (
+        <div className="contact">
+            <span>{props.name}</span>
+        </div>
+    );
+}
+function ContactList(props) {
+    return (
+        <div>
+            {props.contacts.map(c => <Contact key={c.id} name={c.name} />)}
+        </div>
+    );
+}
+
 
 class App extends Component {
     counter = 0;
+    restavracija = "";
+
     activateLasers = () => {
         console.log("lasers activated..");
         this.counter++;
         ReactDOM.render(<App />, document.getElementById('root'));
     }
 
+    izberiRestavracijo = () => {
+        this.restavracija = "bla";
+    }
 
   render() {
+      const contacts = [
+          { id: 1, name: "Laterna" },
+          { id: 2, name: "Kitajska" },
+          { id: 3, name: "Špar" },
+          { id: 4, name: "Via bona" }
+      ];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn how to activate lasers.
-          </a>
+            <ContactList contacts={contacts} />
+
+            <button id={"izberi"} onClick={this.izberiRestavracijo}>
+                IZBERI RESTAVRACIJO  {this.restavracija}
+            </button>
+
             <button id={"mojgumb"} onClick={this.activateLasers}>
                 Activate Lasers  {this.counter}
             </button>
+
         </header>
       </div>
     );
